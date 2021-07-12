e545e16e0617a5072ba6b670b007932f0d237ab7
Mniekyh <m.v.kowalski@gmail.com>
2021-07-12 12:37:27 +0200

    We have homePage

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,48 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Default page of Laravel
 Route::get('/', function () {
     return view('welcome');
 });
+
+//MyPage - string
+Route::get('/home', function ()
+ {
+
+   return view ("home"); 
+
+});
+
+// //MyPage - array (JSON)
+
+// Route::get('/users', function()
+// {
+
+//     return ['PHP', 'HRTML', 'Css '];
+
+// });
+
+
+// //MyPage - JSON object
+
+// Route::get('/users', function ()
+//  {
+//     return response()->json(
+//         ['name' => 'Jakub',
+//         'spec' => "Mechanic",
+//         'example' => "Stupid"
+//     ]
+//     );
+
+
+// });
+
+// //MyPage - Function
+
+// Route::get('/users', function () {
+    
+
+//     return redirect('/');
+
+// });
