cb5bbc3007e99f8c9d7f6809dbf423e1f4d4be4d
Mniekyh <m.v.kowalski@gmail.com>
2021-07-13 18:46:39 +0200

    PAGE AND ID PRODUCT

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,30 @@ use App\Http\Controllers\ProductsController;
 //Laravel 8 -- New Way
 Route::get('/products', [ProductsController::class,'index']);
 
+//Gets products from array
+//Route::get('/products/{name}',[ProductsController::class,'show']);
+
+//Gets products which are intiger & are between 1-oo included
+// Route::get('/products/{id}', [ProductsController::class,'show'])->where('id', '[1-9]+');
+
+//Gets productt which is string
+// Route::get('/products/{name}', [ProductsController::class,'show'])->where('name', '[a-zA-Z]+');
+
+
+
+//Gets page via name and id
+//Like page.com/product/2
+
+// Route::get('/products/{name}/{id}',
+//   [ProductsController::class,'show'])->where([
+//       'name' => '[a-z]+',
+//        'id' => '[0-9]+'
+  
+//     ]);
+
+
+
+
 
 
 //##################### EXAMPLES #######################################
